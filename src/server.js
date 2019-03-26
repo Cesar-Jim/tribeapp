2eca5c7a0db485cb15c49421bc0647b58fe5e98a
Cesar Jimenez <cijgr@hotmail.com>
2019-03-25 22:39:35 -0600

    Landing page ready and routing, controller and view structure

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const http = require("http");
 
 const port = normalizePort(process.env.PORT || "3000");
 
+const server = http.createServer(app);
+
 server.listen(port);
 
 function normalizePort(val) {
